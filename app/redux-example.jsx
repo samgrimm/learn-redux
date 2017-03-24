ed6713fcd5f846a1ec4e79b33cae7117655d2f13
samgrimm <samantha.grimm@gmail.com>
2017-03-24 13:32:40 -0500

    added action to change search text"

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,9 +4,27 @@ console.log('Starting redux');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
   return state;
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Samantha'
+}
+store.dispatch(action);
+
+console.log('Name should be Sam', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -10,9 +10,28 @@ var stateDefault = {
 
 var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
+
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
   return state;
+
 };
 var store = redux.createStore(reducer);
 
+
+
+
+var action = {
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Samantha'
+}
+store.dispatch(action);
 var currentState = store.getState();
-console.log('currentState', currentState);
+console.log('New search text should be Sam', currentState);
