936385ecf84733db175d1ee0751f2baf85242dab
samgrimm <samantha.grimm@gmail.com>
2017-03-25 13:30:45 -0500

    added actions for adding and removing items from arrays with redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,16 @@ var redux = require('redux');
 
 console.log('Starting redux');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
 
   switch (action.type) {
@@ -10,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
       return {
         ...state,
         name: action.name
+      };
+      case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+      case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre,
+          }
+        ]
+      };
+      case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      };
+      case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
       };
     default:
       return state;
@@ -25,6 +67,7 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+  console.log('New state', store.getState());
 });
 
 
@@ -37,6 +80,22 @@ var action = {
 }
 store.dispatch(action);
 
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'walking'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Logan',
+  genre: 'Action'
+});
+
 //unsubscribe();
 
 store.dispatch({
@@ -45,3 +104,19 @@ store.dispatch({
 });
 
 console.log('Name should be Sam', store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Star Trek',
+  genre: 'Sci-Fi'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
